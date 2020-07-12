6253d5a00109b0ce70d8602a185725bf7246d6d9
ishpiamazing <ishpiamazing@gmail.com>
2020-07-12 17:29:52 +0000

    added file odd.js

---
+++ odd.js
@@ -0,0 +1,9 @@
+const isOdd = function(num){
+  return num % 2 !== 0 ;
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
+
+console.log("11 is odd: " + isOdd(11));
+console.log("20 is odd: " + isOdd(20));
